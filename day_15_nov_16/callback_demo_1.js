c4a723dfb900aac61e9bcf6380c13f8baed97c30
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-11-18 08:26:07 +0530

    day 16

--- day_15_nov_16/callback_demo_1.js
+++ day_15_nov_16/callback_demo_1.js
@@ -3,9 +3,9 @@ let arr = [10, 20, 30, 40, 50];
 function incrementBy5(ele, ind, arr) { // callback function
     return ele + 5;
 }
-let resArr = arr.map(incrementBy5);
+let resArr = arr.map(incrementBy5); // map() - HigherOrder , incrementBy5() - callBack
 console.log(resArr);
 
 // find the sum of all the numbers
-let sum = arr.reduce((sum,ele)=>sum+ele);
+let sum = arr.reduce((sum,ele)=>sum+ele); // reduce()-HigherOrder , arrow function is the Callback
 console.log(sum);
