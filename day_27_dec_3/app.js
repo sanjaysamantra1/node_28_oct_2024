5656085d70af86e1b6d0c35fdeaa817e547d957a
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-12-03 07:50:36 +0530

    added

--- day_27_dec_3/app.js
+++ day_27_dec_3/app.js
@@ -3,8 +3,8 @@ const employeeRouter = require('./routes/employee_routes');
 const productRouter = require('./routes/product_routes');
 const app = express();
 
-app.use('/employees',employeeRouter)
-app.use('/products',productRouter)
+app.use('/employees',employeeRouter);
+app.use('/products',productRouter);
 
 app.listen(5000, () => {
     console.log(`Server Running at 5000 port`)
