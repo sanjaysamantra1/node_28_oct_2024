f5d03838befd4723c09b8579ee35fef77ffc4693
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-12-07 08:05:08 +0530

    ejs demo

--- day_31_dec_7/ejs_demo/routes/users.js
+++ day_31_dec_7/ejs_demo/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const usersArr = require('../../../data/users.json');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.render('users', { usersArr, title: 'User List' })
 });
 
 module.exports = router;
